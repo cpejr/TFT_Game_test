3e5dbc6923152d8e94498771e2438b6d14d2feec
bruno-apinto <brunoapinto2004@gmail.com>
2024-04-05 07:11:25 -0300

    Mudei os typedefs e consertei os parametros de fill color e setcolor

--- include/Display.hpp
+++ include/Display.hpp
@@ -13,7 +13,8 @@
 #include <Adafruit_GFX.h>
 #include <TFT_ILI9163C.h>
 
-typedef uint8_t mInt;
+typedef uint8_t mini8;
+typedef uint16_t mini16;
 
 class Rectangle
 {
@@ -24,10 +25,10 @@ class Rectangle
         TFT_ILI9163C& display; //Display de referência onde será exibido o objeto
 
     public:
-        Rectangle (mInt cx, mInt cy, mInt ch, mInt cl, uint16_t cColor, TFT_ILI9163C& Display_); //construtor da classe
-        void setPosition(mInt x_, mInt y_); //Define a posição do objeto no display
-        void setColor(mInt color_); //Define a cor do objeto
-        void fillColor(mInt color_); //Preenche o retângulo com a cor especificada
+        Rectangle (mini8 cx, mini8 cy, mini8 ch, mini8 cl, mini16 cColor, TFT_ILI9163C& Display_); //construtor da classe
+        void setPosition(mini8 x_, mini8 y_); //Define a posição do objeto no display
+        void setColor(mini16 color_); //Define a cor do objeto
+        void fillColor(mini16 color_); //Preenche o retângulo com a cor especificada
         void fillColor(); //Preenche o retângulo com a cor dfinida pro objeto quando nenhuma cor adicional é especificada
         void incrementX();
         void decrementX();
--- src/Display.cpp
+++ src/Display.cpp
@@ -1,18 +1,18 @@
 #include "Display.hpp"
 
-Rectangle::Rectangle (mInt cx, mInt cy, mInt ch, mInt cl, uint16_t cColor, TFT_ILI9163C& Display_) :  
+Rectangle::Rectangle (mini8 cx, mini8 cy, mini8 ch, mini8 cl, uint16_t cColor, TFT_ILI9163C& Display_) :  
     x(cx), y(cy), h(ch), l(cl), color(cColor), display(Display_) {}
 
-void Rectangle::setPosition(mInt x_, mInt y_){
+void Rectangle::setPosition(mini8 x_, mini8 y_){
     this->x = x_;
     this->y = y_;
     }
 
-void Rectangle::setColor(mInt color_){
+void Rectangle::setColor(mini16 color_){
     this->color = color_;
     }
 
-void Rectangle::fillColor(mInt color_){
+void Rectangle::fillColor(mini16 color_){
     this->display.fillRect(this->x,this->y,this->l,this->h,color_);
     }
 
